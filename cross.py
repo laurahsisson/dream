cfd79d3f38d07b414095c6a46cfdc5513e4d5a98
Laura Sisson <laura.h.sisson@gmail.com>
2024-07-27 13:53:02 -0400

    More refactoring

---
+++ cross.py
@@ -0,0 +1,44 @@
+import olfactor
+
+class CrossEncoder(torch.nn.Module):
+    def __init__(self,olfactor:olfactor.Olfactor,representation_mode: str, cross_encoder_dim: int, act_mode: str, do_encoder_diff: bool,**kwargs):
+        super(CrossEncoder, self).__init__()
+        self.olfactor = olfactor
+        self.representation_mode = representation_mode
+        if self.representation_mode == "embed":
+          self.input_dim = self.olfactor.embed_dim
+        if self.representation_mode == "notes":
+          self.input_dim = self.olfactor.notes_dim
+        if self.representation_mode == "both":
+          self.input_dim = self.olfactor.embed_dim + self.olfactor.notes_dim
+
+        self.do_encoder_diff = do_encoder_diff
+        if self.do_encoder_diff:
+          combined_dim = self.input_dim
+        else:
+          combined_dim = 2*self.input_dim
+
+        act_fn = dream.activation.get_act_fn(act_mode)
+        if cross_encoder_dim > 0:
+          self.readout = torch.nn.Sequential(torch.nn.Linear(combined_dim,cross_encoder_dim),act_fn(),
+                                            torch.nn.Linear(cross_encoder_dim,1))
+        else:
+          self.readout = torch.nn.Linear(combined_dim,1)
+
+    def get_representation(self,graph):
+      olf = self.olfactor(graph)
+      if self.representation_mode == "embed":
+        return olf["embed"]
+      if self.representation_mode == "notes":
+        return olf["logits"]
+      return torch.cat([olf["embed"],olf["logits"]],dim=-1)
+
+    def forward(self,graph1, graph2):
+      repr1 = self.get_representation(graph1)
+      repr2 = self.get_representation(graph2)
+      if self.do_encoder_diff:
+        x = (repr1-repr2).square()
+      else:
+        x = torch.cat([repr1,repr2],dim=-1)
+
+      return self.readout(x).squeeze(dim=-1)
---
+++ olfactor.py
@@ -0,0 +1,15 @@
+import encoder
+import activation
+
+class Olfactor(torch.nn.Module):
+    def __init__(self, encoder: encoder.Encoder, notes_dim: int, **kwargs):
+        super(Olfactor, self).__init__()
+        self.encoder = encoder
+        self.embed_dim = self.encoder.readout.in_channels
+        self.notes_dim = notes_dim
+        self.predict = torch.nn.Linear(self.embed_dim,self.notes_dim)
+
+    def forward(self,graph):
+        embed = self.encoder(graph)
+        logits = self.predict(embed)
+        return {"embed":embed,"logits":logits}
