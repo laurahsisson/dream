789a3ec6b50d3429594f129bbd669b632a2e7d71
Laura Sisson <laura.h.sisson@gmail.com>
2024-07-28 12:36:44 -0400

    refactor

--- utils.py
+++ utils.py
@@ -4,9 +4,17 @@ import torch
 def count_parameters(module: torch.nn.Module):
     return sum(p.numel() for p in module.parameters())
 
-
 def readout_counts(module: torch.nn.Module):
     results = {"total": count_parameters(module)}
     for n, c in module.named_children():
         results[n] = count_parameters(c)
     return results
+
+def make_scheduler(optimizer, warmup, total_steps):
+  warmup_steps = int(warmup * total_steps)
+  cooldown_steps = total_steps - warmup_steps
+  # Define the warmup and cooldown schedulers
+  warmup_scheduler = torch.optim.lr_scheduler.LinearLR(optimizer, start_factor=1e-10, end_factor=1.0, total_iters=warmup_steps)
+  cooldown_scheduler = torch.optim.lr_scheduler.LinearLR(optimizer, start_factor=1.0, end_factor=1e-10, total_iters=cooldown_steps)
+  # Chain the schedulers together with SequentialLR
+  return torch.optim.lr_scheduler.SequentialLR(optimizer, schedulers=[warmup_scheduler, cooldown_scheduler], milestones=[warmup_steps])
