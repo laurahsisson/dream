d497af4e35ecf7ea2de5d69e9ef29c49c030b452
Laura Sisson <laura.h.sisson@gmail.com>
2024-12-16 16:36:08 -0500

    device

--- production.py
+++ production.py
@@ -6,7 +6,7 @@ import torch
 # Get the directory where this script is located
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-def _load_model(model_path):
+def _load_model(model_path, device='cuda'):
     # Build an absolute path from the script location
     production_path = os.path.join(BASE_DIR, "Production", model_path)
 
@@ -14,7 +14,7 @@ def _load_model(model_path):
         config = json.load(f)
 
     graph_model = gcn.GCN(**config)
-    model_weights = torch.load(os.path.join(production_path, "model.pt"), weights_only=True)
+    model_weights = torch.load(os.path.join(production_path, "model.pt"), weights_only=True, map_location=device)
     graph_model.load_state_dict(model_weights)
     graph_model.eval()
     return graph_model, config
