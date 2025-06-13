64a7dde0a71bc42d3c5be4ffc7a5caf8c0eeb3c7
Laura Sisson <laura.h.sisson@gmail.com>
2025-06-13 18:57:14 -0400

    also installing models

--- setup.py
+++ setup.py
@@ -1,11 +1,19 @@
-from setuptools import find_packages, setup
-
 setup(
-    name="odor-pair",  # pip install name (can contain dash)
+    name="odor-pair",
     version="1.0",
     author="Laura Sisson",
     description="Tools for modeling odorant pairs",
     packages=find_packages(include=["odorpair", "odorpair.*"]),
     python_requires=">=3.11",
-    install_requires=["torch", "torch-geometric", "rdkit", "ogb", "numpy"],
+    install_requires=[
+        "torch",
+        "torch-geometric",
+        "rdkit",
+        "ogb",
+        "numpy"
+    ],
+    include_package_data=True,
+    package_data={
+        "odorpair": ["Production/*/*.json", "Production/*/*.pt"],
+    },
 )
